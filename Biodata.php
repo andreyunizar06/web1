99da6844af4f5f441e3ae30b1c9eef980851e718
andreyunizar06 <mandreynz14@gmail.com>
2025-01-25 20:35:39 +0700

    Update Biodata.php

--- Biodata.php
+++ Biodata.php
@@ -4,6 +4,7 @@
      if(!isset($_SESSION['email'])){
      header('Location: Login.php');
      }
+          $teks = "HALOOOOO, SELAMAT DATANG !!!";
      ?>
 <html>
     <head>
@@ -21,6 +22,43 @@
     </head>
 
 <body>
+     <style>
+    
+    .marquee {
+        overflow: hidden;
+        white-space: nowrap;
+        position: relative;
+        width: 50%; 
+        margin: 20px auto; 
+        background-color:rgb(200, 247, 255);
+        border: 1px solid #ccc;
+        padding: 10px; 
+        box-shadow: 0 4px 6px rgb(255, 255, 255);
+    }
+
+    
+    .marquee span {
+        display: inline-block;
+        padding-left: 100%;
+        animation: moveText 6s linear infinite;
+    }
+
+    
+    @keyframes moveText {
+        0% {
+            transform: translateX(100%);
+        }
+        100% {
+            transform: translateX(-100%);
+        }
+    }
+</style>
+</head>
+<body>
+<div class="marquee">
+    <span><?php echo $teks; ?></span>
+</div>
+     
 <div class="container-fluid">
     <div class="text-center px-4 py-5">
      <img src="assets/kucing.jpg" width="100" height="100">
